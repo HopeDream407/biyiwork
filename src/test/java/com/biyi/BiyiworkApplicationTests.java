30ea65823b7fa90987ddc21b7a07fe563496e60f
litan <946402109@qq.com>
2021-09-14 13:31:14 +0800

    Initial commit

---
+++ src/test/java/com/biyi/BiyiworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biyi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiyiworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
